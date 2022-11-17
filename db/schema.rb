18298efea79908ff0bf6dabb24861f968e78db95
Zayyana Choir <zaycho_89@yahoo.com>
2022-11-17 13:39:40 +0700

    add column wallet_id to wallet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_091635) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_063811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_091635) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "wallet_id"
     t.index ["user_id"], name: "index_wallets_on_user_id"
   end
 
